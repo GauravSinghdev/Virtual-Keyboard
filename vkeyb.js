a3dda9c35245f403adc7fbe6c634e26379801efc
GauravSinghdev <gauravkalakoti19@gmail.com>
2024-04-04 02:14:58 +0530

    first commit

---
+++ vkeyb.js
@@ -0,0 +1,266 @@
+function clean()
+{
+    document.getElementById("txt").innerHTML = "";
+}
+
+function backspace()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    let s  = result.split("")
+    s.pop()
+    s = s.join("")
+
+    document.getElementById("txt").innerHTML = s;
+}
+
+function space()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += " "
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addQ()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "q"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addW()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "w"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addE()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "e"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addR()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "r"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addT()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "t"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addY()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "y"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addU()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "u"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addI()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "i"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addO()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "o"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addP()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "p"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+
+// -------------------------------------------
+
+function addA()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "a"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addS()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "s"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addD()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "d"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+
+function addF()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "f"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+
+function addG()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "g"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addH()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "h"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addJ()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "j"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addK()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "k"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addL()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "l"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+// ---------------------------------------
+
+function addZ()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "z"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addX()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "x"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addC()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "c"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addV()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "v"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addB()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "b"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addN()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "n"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
+function addM()
+{
+    let result = document.getElementById("txt").innerHTML;
+
+    result += "m"
+
+    document.getElementById("txt").innerHTML = result;
+}
+
